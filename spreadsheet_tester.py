fe017b812f281aa8def51a6d6a54517bd9c1a83e
Ragnyll <justanotherjakeyg@gmail.com>
2016-11-03 00:38:00 -0500

    wrote unit test for spreadsheet_geneartor

---
+++ spreadsheet_tester.py
@@ -0,0 +1,23 @@
+"""
+This file makes sure that the data in the spread sheet was generated correctly
+
+Tests spreadsheet_generator.py
+"""
+import json
+from os import listdir
+from os.path import isfile, join
+
+if __name__ == '__main__':
+    root_dir = './spreadsheets/'
+    spreadsheets = [f for f in listdir(root_dir) if isfile(join(root_dir, f))]
+
+    # make sure that each json object is valid
+    for spreadsheet in spreadsheets:
+        with open(root_dir + spreadsheet, 'r') as json_data:
+            d = json.load(json_data)
+            ref = {}
+            try:
+                if not type(d) == type(ref):
+                    raise TypeError
+            except TypeError:
+                print('The generated json file was not valid')
