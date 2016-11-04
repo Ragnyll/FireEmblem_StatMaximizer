2acf82c39df42d79669b8a99b09bd56e884c4cba
Ragnyll <justanotherjakeyg@gmail.com>
2016-11-04 11:07:30 -0500

    stat_gains_to_max() works

--- calculate_growth_stats.py
+++ calculate_growth_stats.py
@@ -19,6 +19,7 @@ def calc_levels_left(char_name):
 
 def calc_stat_gains_to_max(char_name):
     """Calculates the number of stat gains need for a character to max out all stats
+    NOTE: subracts out stats gained on a class up!
 
     param char_name: string denoting the character name
 
@@ -27,11 +28,25 @@ def calc_stat_gains_to_max(char_name):
     with open(SPREADSHEET_PATH + 'character_stat_caps.json', 'r') as stat_caps_file:
         char_stat_caps = json.load(stat_caps_file)[char_name]
 
-    with open(SPREADSHEET_PATH + 'character_stat_caps.json', 'r') as starting_stats_file:
+    with open(SPREADSHEET_PATH + 'starting_stats.json', 'r') as starting_stats_file:
         char_starting_stats = json.load(starting_stats_file)[char_name]
 
+    with open(SPREADSHEET_PATH + 'class_change_bonuses.json', 'r') as class_change_file:
+        class_change_stats = json.load(class_change_file)
+        if char_name in class_change_stats:
+            class_change_stats = class_change_stats[char_name]
+        else:
+            class_change_stats = None
+
+    for stat_type in char_stat_caps:
+        if class_change_stats is not None:
+            char_stat_caps[stat_type] = char_stat_caps[stat_type] - char_starting_stats[stat_type] - class_change_stats[stat_type]
+        else:
+            char_stat_caps[stat_type] = char_stat_caps[stat_type] - char_starting_stats[stat_type]
+
+    return char_stat_caps
 
 if __name__ == '__main__':
     character = input('Which character\'s stats do you want to max? \n')
     levels_left = calc_levels_left(character)
-    calc_stat_gains_to_max(character)
+    gains_to_max = calc_stat_gains_to_max(character)
